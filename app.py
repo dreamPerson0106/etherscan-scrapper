036d995820539b408749a4b85f9bc5a1289ac069
DreamPerson <dreamPerson0106@gmail.com>
2023-08-26 23:06:22 +0900

    asdf

--- app.py
+++ app.py
@@ -1,32 +1,46 @@
 from flask import Flask, make_response, jsonify
-from undetected_chromedriver import Chrome, ChromeOptions
+from selenium.webdriver import Chrome, ChromeOptions
+import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 import time
-
-app = Flask(__name__)
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = None
 options = ChromeOptions()
-options.add_argument("--headless")
+# options.add_argument("--headless")
+options.add_argument("--no-sandbox")
 driver = Chrome(options=options)
+driver = uc.Chrome()
+driver.get(f'https://etherscan.io')
+time.sleep(3)
+driver.execute_script('location.reload();')
+try:
+    # element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "#challenge-stage input[type='checkbox']")))
+    time.sleep(1)
+    element = driver.find_element(By.CSS_SELECTOR, "#challenge-stage input[type='checkbox']")
+    print(element)
+    element.click()
+except Exception as error:
+    print(error)
+    pass
+
+
 def interceptor(request):
     request.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
     request.headers['Cookie'] = '_ga=GA1.1.1422679124.1689979108; etherscan_cookieconsent=True; ASP.NET_SessionId=mw4gm0cu3qe3e4byj3ddqlpv; __cflb=0H28vPcoRrcznZcNZSuFrvaNdHwh857mdFQeeFybDTa; cf_clearance=6Lz94hEhOqTKxgOr8y28Gvhb7vCaupsn_IbIR9SquQs-1693022025-0-1-ba6f4700.1bb4a04c.4478fa33-0.2.1693022025; _ga_T1JC9RNQXV=GS1.1.1693030896.5.1.1693033089.0.0.0'
 
 
+app = Flask(__name__)
+
 @app.route('/<hash>')
 def get_info(hash):
-    print(hash)
     driver.execute_script("window.open('');")
     driver.switch_to.window(driver.window_handles[1])
     driver.get(f'https://etherscan.io/tx/{hash}')
     print(driver.page_source)
     try: 
         driver.find_element(By.CSS_SELECTOR, '#ContentPlaceHolder1_maintable .card:first-child #ContentPlaceHolder1_divTimeStamp span[data-bs-toggle]')
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
         return jsonify({"status": True})
     except:
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
         return jsonify({"status": False})
---
+++ main.py
@@ -0,0 +1,31 @@
+import undetected_chromedriver as uc
+from selenium.webdriver import ChromeOptions, Chrome
+import time
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+chrome_options = ChromeOptions()
+chrome_options.add_argument(
+                        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
+chrome_options.add_argument('--log-level=3')
+chrome_options.add_experimental_option(
+    "useAutomationExtension", False)
+chrome_options.add_experimental_option(
+    "excludeSwitches", ["enable-automation"])
+driver = Chrome(options=chrome_options)
+driver.get("https://etherscan.io")
+
+try:
+        WebDriverWait(driver,10).until(EC.frame_to_be_available_and_switch_to_it(
+            (By.CSS_SELECTOR, "iframe[title='Widget containing a Cloudflare security challenge']")))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+            (By.CSS_SELECTOR, "label.ctp-checkbox-label"))).click()
+except Exception as error:
+    print(error)
+
+
+
+print(driver.page_source)
+time.sleep(100)
