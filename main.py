f111764743ba77f6f00bb3c53438c0600580391e
DreamPerson <dreamPerson0106@gmail.com>
2023-08-26 23:24:18 +0900

    123

--- main.py
+++ main.py
@@ -7,10 +7,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 try:
     chrome_options = ChromeOptions()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument(
-    #                         "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-    
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument(
+                            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
+    chrome_options.add_argument('--log-level=3')
+    chrome_options.add_experimental_option(
+        "useAutomationExtension", False)
+    chrome_options.add_experimental_option(
+        "excludeSwitches", ["enable-automation"])
     driver = Chrome(options=chrome_options)
     driver.get("https://etherscan.io")
 
@@ -23,6 +27,6 @@ except Exception as error:
     print(error)
 
 
-
+time.sleep(5)
 print(driver.page_source)
 time.sleep(100)
